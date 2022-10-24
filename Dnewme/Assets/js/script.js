b06ddbecdb5d5000ad73aedb9fabee37ce415c22
shinejoy14 <107547246+shinejoy14@users.noreply.github.com>
2022-10-24 22:31:17 +0530

    Delete Dnewme directory

--- Dnewme/Assets/js/script.js
+++
@@ -1,7 +0,0 @@
-    const currentLocation = location.href;
-    const navItem = document.querySelectorAll("a");
-    for(let i=0; i < navItem.length; i++){
-        if(navItem[i].href == currentLocation){
-            navItem[i].className = "current";
-        };
-    };
